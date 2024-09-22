c5b259363c41ab5c125ca03a62bfea1a9d50ff0f
cygnus07 <kuldeepsingh21070@gmail.com>
2024-09-22 15:33:22 +0530

    completed the nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-lg bg-yellow-100"> This is Aora</Text>
+    </View>
+  );
+}

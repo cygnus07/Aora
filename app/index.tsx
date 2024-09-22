43f26f532b9bb56871983c2a990933290a4c98b0
cygnus07 <kuldeepsingh21070@gmail.com>
2024-09-22 17:04:09 +0530

    structured the folders and defined the tabs layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from 'expo-router'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-lg bg-yellow-100"> This is Aora</Text>
+    <View className="flex-1 items-center justify-center bg-black-100">
+      <Text className="text-3xl font-pblack text-white"> This is Aora</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: 'white' }}>Go to Home</Link>
     </View>
   );
 }
